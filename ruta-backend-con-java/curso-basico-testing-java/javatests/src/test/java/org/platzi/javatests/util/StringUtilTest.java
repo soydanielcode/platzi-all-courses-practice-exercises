20e55d71fc639908020edf6d9cf55a0823478436
soydanielcode <soydanielcode@outlook.com>
2023-11-30 15:24:24 -0500

    Add Library JUnit with examples

--- ruta-backend-con-java/curso-basico-testing-java/javatests/src/test/java/org/platzi/javatests/util/StringUtilTest.java
+++ ruta-backend-con-java/curso-basico-testing-java/javatests/src/test/java/org/platzi/javatests/util/StringUtilTest.java
@@ -1,13 +1,36 @@
 package org.platzi.javatests.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StringUtilTest {
     public static void main(String[] args) {
+        repeatTextManualTest();
+    }
+
+    @Test
+    public void assertEqualsJUnit(){
+        Assert.assertEquals("holaholahola", StringUtil.repeat("hola",3));
+        Assert.assertEquals("byby", StringUtil.repeat("by",4));
+    }
+
+    public static void assertEqualsManual(String actual, String expected){
+        if (!actual.equals(expected)){
+            throw new RuntimeException(actual + " is not equals to " + expected);
+        }
+    }
+    public static void repeatTextManualTest(){
         String text = StringUtil.repeat("Hola", 5);
         if (text.equals("HolaHolaHolaHolaHola")){
             System.out.println("ok");
         }
+
         String text1 = StringUtil.repeat("Hola", 3);
         if (!text1.equals("HolaHolaHolaBy")){
             System.out.println("Error");
         }
+
+        String text3 = StringUtil.repeat("Hola", 2);
+        assertEqualsManual(text3, "HolaHolaBy");
     }
 }
