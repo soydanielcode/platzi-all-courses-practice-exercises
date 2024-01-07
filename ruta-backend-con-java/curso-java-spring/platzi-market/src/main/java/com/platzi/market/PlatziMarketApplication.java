88c5347ac0e82a55b3fe5d2b79bb587a05dcbd7a
soydanielcode <soydanielcode@outlook.com>
2024-01-07 13:46:37 -0500

    Add of swagger new version springdoc-openapi-starter-webmvc

--- ruta-backend-con-java/curso-java-spring/platzi-market/src/main/java/com/platzi/market/PlatziMarketApplication.java
+++ ruta-backend-con-java/curso-java-spring/platzi-market/src/main/java/com/platzi/market/PlatziMarketApplication.java
@@ -3,7 +3,7 @@ package com.platzi.market;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class PlatziMarketApplication {
 
 	public static void main(String[] args) {
--- ruta-backend-con-java/curso-java-spring/platzi-market/src/main/java/com/platzi/market/web/controller/PurchaseController.java
+++ ruta-backend-con-java/curso-java-spring/platzi-market/src/main/java/com/platzi/market/web/controller/PurchaseController.java
@@ -2,6 +2,9 @@ package com.platzi.market.web.controller;
 
 import com.platzi.market.domain.Purchase;
 import com.platzi.market.domain.service.PurchaseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.models.annotations.OpenAPI31;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,10 +20,14 @@ public class PurchaseController {
     PurchaseService service;
 
     @GetMapping("/all")
+    @Operation(summary = "Get all purchases")
+    @ApiResponse(responseCode = "200", description = "OK")
     private ResponseEntity<List<Purchase>> getAll(){
         return new ResponseEntity<>(service.getAll(), HttpStatus.OK);
     }
 
+    @Operation(summary = "Get purchases by client Id")
+    @ApiResponse(responseCode = "200", description = "OK")
     @GetMapping("/client/{clientId}")
     private ResponseEntity<List<Purchase>> getByClient(@PathVariable("clientId") String clientId) {
         return service.getByClient((clientId))
@@ -28,6 +35,8 @@ public class PurchaseController {
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    @Operation(summary = "To Save a purchase")
+    @ApiResponse(responseCode = "200", description = "Created")
     @PostMapping("/save")
     private ResponseEntity<Purchase> save(@RequestBody Purchase purchase){
         return new ResponseEntity<>(service.save(purchase), HttpStatus.CREATED);
