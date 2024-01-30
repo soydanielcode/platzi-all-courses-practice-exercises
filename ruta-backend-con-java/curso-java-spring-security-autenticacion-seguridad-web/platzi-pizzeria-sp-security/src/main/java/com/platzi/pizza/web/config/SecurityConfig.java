b0ae1f84c5552bf34f49a4a37a98c8ec521f2a27
soydanielcode <soydanielcode@outlook.com>
2024-01-30 11:59:44 -0500

    Add Authentication in Memory

--- ruta-backend-con-java/curso-java-spring-security-autenticacion-seguridad-web/platzi-pizzeria-sp-security/src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
+++ ruta-backend-con-java/curso-java-spring-security-autenticacion-seguridad-web/platzi-pizzeria-sp-security/src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
@@ -2,7 +2,14 @@ package com.platzi.pizza.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,11 +19,28 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .cors().and()
                 .authorizeHttpRequests()
+                .requestMatchers(HttpMethod.GET, "/api/*").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService memoryUsers() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
