64500ee730960262da8f9957f8cca8f87f68db14
soydanielcode <soydanielcode@outlook.com>
2024-01-30 17:43:11 -0500

    Add method to add Authorities to Roles

--- ruta-backend-con-java/curso-java-spring-security-autenticacion-seguridad-web/platzi-pizzeria-sp-security/src/main/java/com/platzi/pizza/service/UserSecurityService.java
+++ ruta-backend-con-java/curso-java-spring-security-autenticacion-seguridad-web/platzi-pizzeria-sp-security/src/main/java/com/platzi/pizza/service/UserSecurityService.java
@@ -1,13 +1,19 @@
 package com.platzi.pizza.service;
 
 import com.platzi.pizza.persistence.entity.UserEntity;
+import com.platzi.pizza.persistence.entity.UserRoleEntity;
 import com.platzi.pizza.persistence.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserSecurityService implements UserDetailsService {
@@ -21,12 +27,31 @@ public class UserSecurityService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserEntity user = repository.findById(username).orElseThrow(() -> new UsernameNotFoundException("User " + username + " not found"));
+        String[]roles = user.getRoles().stream().map(UserRoleEntity::getRole).toArray(String[]::new);
         return User.builder()
                 .username(user.getUsername())
                 .password(user.getPassword())
-                .roles("ADMIN")
+                .authorities(grantedAuthorities(roles))
                 .accountLocked(user.getLocked())
                 .disabled(user.getDisabled())
                 .build();
+    }
+
+    private String[] getAuthorities(String role) {
+        if("ADMIN".equals(role) || "CUSTOMER".equals(role)) {
+            return new String[] {"random_order"};
+        }
+        return new String[]{};
+    }
+
+    private List<GrantedAuthority> grantedAuthorities(String[] roles) {
+        List<GrantedAuthority> authorities = new ArrayList<>(roles.length);
+        for (String role: roles){
+            authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
+            for(String authority: getAuthorities(role)){
+                authorities.add(new SimpleGrantedAuthority(authority));
+            }
+        }
+        return authorities;
     }
 }
